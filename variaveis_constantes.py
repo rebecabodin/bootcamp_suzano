277f7420248746853dce07cd729acaaf90c13954
rebecabodin <rebecabodin@gmail.com>
2025-09-02 21:28:40 -0300

    feat: implementação de novas funcionalidades

---
+++ variaveis_constantes.py
@@ -0,0 +1,14 @@
+nome = "Guilherme"
+idade = 28
+
+nome, idade = "Giovana", 27
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZIL_STATES = ["SP", "RJ", "SC", "RS"] 
+
+BRAZIL_STATES = 10
+
+print(BRAZIL_STATES)
